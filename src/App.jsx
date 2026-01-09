e3c82e02378e55862a6fabd580c9afce46d48354
Amarnath39 <151598632+Amarnath39@users.noreply.github.com>
2026-01-09 09:54:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-     <h1>This is from jenkins pipeline v23</h1>
+     <h1>This is from jenkins pipeline v25</h1>
     </div>
   )
 }
